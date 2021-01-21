58368ad8128769548fbb1a538187881e349715cc
Christalee Bieber <cbieber@alum.mit.edu>
2021-01-21 15:31:21 -0500

    Markers are color-coded, with a legend

--- l-demo.js
+++ l-demo.js
@@ -1,4 +1,5 @@
-let mymap = L.map('mapid').setView([51.505, -0.09], 2);
+// Start by instantiating a map and using ESRI tiles
+let mymap = L.map('mapid').setView([0, 0], 2);
 
 const Esri_WorldStreetMap = L.tileLayer(
   'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
@@ -7,44 +8,85 @@ const Esri_WorldStreetMap = L.tileLayer(
 
 Esri_WorldStreetMap.addTo(mymap);
 
+// create all the markers and collect them in an array
 const markers = new Array();
-const mark = function (coords, popup) {
-  const c = L.circle(coords, {
-    color: 'red',
-    fillColor: 'red',
-    radius: 1000
+const mark = function (coords, popup, color) {
+  const c = L.circleMarker(coords, {
+    weight: 1,
+    color: "black",
+    fillColor: color,
+    fillOpacity: 1,
+    radius: 3
   });
 
   c.bindPopup(popup);
   markers.push(c);
 }
 
+// A helper fn to return the appropriate color for each band of prices
+const colors = ['#ffffd4', '#fed98e', '#fe9929', '#d95f0e', '#993404'];
+
+let bins = new Array();
+
+function make_bins() {
+  const prices = Object.values(raw.price)
+  const price_range = [Math.min(...prices), Math.max(...prices)]
+  const price_interval = (price_range[1] - price_range[0]) / 4;
+  for (let x = 0; x < 5; x += 1) {
+    let cut = Math.round((price_range[0] + x * price_interval) * 100) / 100;
+    bins.push(cut);
+  }
+}
+
+function m_color(val) {
+  if (val >= bins[0] && val < bins[1]) {
+    return colors[0];
+  }
+  if (val >= bins[1] && val < bins[2]) {
+    return colors[1];
+  }
+  if (val >= bins[2] && val < bins[3]) {
+    return colors[2];
+  }
+  if (val >= bins[3] && val < bins[4]) {
+    return colors[3];
+  } else {
+    return colors[4];
+  }
+}
+
+let legend = L.control({
+  position: 'bottomright'
+});
+
+legend.onAdd = function (map) {
+  let div = L.DomUtil.create('div', 'info legend');
+
+  // loop through our density intervals and generate a label with a colored square
+  // for each interval
+  for (let i = 0; i < colors.length; i++) {
+    div.innerHTML += '<i style="background: ' + colors[i] + '"></i> ' +
+      String(bins[i]) + (bins[i + 1] ? ' &ndash; ' + String(bins[i + 1]) +
+        '<br>' : '+');
+    // div.innerHTML += '<i style="background:' + colors[i] + '"></i> ' +
+    //   bins[i] + (bins[i + 1] ? '&ndash;' + bins[i + 1] + '<br>' : '+');
+  }
+
+  return div;
+};
+
+// To filter the markers on the screen to the top 50 (?) by population,
+// you need a list of what's currently on the screen, and a way to sort them
 function between(marker, bounds) {
   const lat = [bounds.getSouth(), bounds.getNorth()];
   const lng = [bounds.getWest(), bounds.getEast()];
   const m_latlng = marker.getLatLng();
-  if (m_latlng.lat >= lat[0] && m_latlng.lat <= lat[1] && m_latlng.lng >= lng[0] &&
+  if (m_latlng.lat >= lat[0] && m_latlng.lat <= lat[1] && m_latlng.lng >=
+    lng[0] &&
     m_latlng.lng <= lng[1]) {
     return true;
   } else {
     return false;
-  }
-}
-
-function onMapZoom(e) {
-  const bounds = mymap.getBounds();
-  // console.log(bounds);
-  let inbounds = new Array();
-  for (const m of markers) {
-    m.remove()
-    if (between(m, bounds)) {
-      inbounds.push(m);
-    }
-  }
-  inbounds.sort((a, b) => inbounds_sort(b) - inbounds_sort(a));
-  console.log(inbounds);
-  for (const x of inbounds.slice(0, 50)) {
-    x.addTo(mymap);
   }
 }
 
@@ -57,9 +99,25 @@ function inbounds_sort(marker) {
   }
 }
 
+function onMapZoom(e) {
+  const bounds = mymap.getBounds();
+  let inbounds = new Array();
+  for (const m of markers) {
+    m.remove()
+    if (between(m, bounds)) {
+      inbounds.push(m);
+    }
+  }
+  inbounds.sort((a, b) => inbounds_sort(b) - inbounds_sort(a));
+  for (const x of inbounds.slice(0, 50)) {
+    x.addTo(mymap);
+  }
+}
+
 // mymap.on("load", onMapZoom);
 mymap.on("zoomend", onMapZoom);
 mymap.on("moveend", onMapZoom);
+
 //
 // fetch('http://localhost:8000/graphs/Seoul.html', {
 //     mode: 'same-origin'
@@ -71,6 +129,7 @@ mymap.on("moveend", onMapZoom);
 //     });
 //   });
 
+// Creates an array of objects, each containing 1 row of data
 function parse(json) {
   const x = Object.entries(json);
   const y = Object.entries(x[0][1]);
@@ -86,22 +145,31 @@ function parse(json) {
   return a;
 };
 
+// Fetches the JSON and dumps it into variables for later use,
+// also draws the markers
+let raw;
 let db_beer;
-fetch('http://localhost:8000/data/deutschebank/beer-clean.json', {
-    mode: 'same-origin'
-  })
+fetch(
+    'http://localhost:8000/data/deutschebank/beer-clean.json', {
+      mode: 'same-origin'
+    })
   .then(response => response.json())
   .then(data => {
-    console.log(data);
+    raw = data;
+    make_bins();
+
     db_beer = parse(data);
     for (const x of db_beer) {
-      mark([x.latitude, x.longitude], String(x.population));
+      let text = String(x.city_ascii) + ": " + String(x.price)
+      mark([x.latitude, x.longitude], text, m_color(x.price));
     }
     for (const y of markers) {
       y.addTo(mymap);
     }
+    legend.addTo(mymap);
   });
 
+// an effort to figure out how to add charts to popups
 const html =
   `<head>
   <style>
@@ -135,7 +203,3 @@ const html =
 
   </script>
 </body>`
-
-// circle.bindPopup(html, {
-//   minWidth: 400
-// })
